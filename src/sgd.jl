ac9ff82792f01a973a04bc0f08cc6c0749cc8c00
Sam Lendle <sam.lendle@gmail.com>
2014-03-27 22:01:51 -0700

    Adding AdaGrad

--- src/sgd.jl
+++ src/sgd.jl
@@ -1,5 +1,5 @@
 
-export AbstractSGD, SimpleSGD, AdaDelta
+export AbstractSGD, SimpleSGD, AdaDelta, AdaGrad
 
 abstract AbstractSGD
 #should implement a update! method:
@@ -43,7 +43,6 @@ AdaDelta(rho::Float64, eps::Float64) = AdaDelta{Float64}(rho, eps)
 
 Base.show{T}(io::IO, obj::AdaDelta{T}) = print(io, "AdaDelta{$T}(rho=$(obj.rho), eps=$(obj.eps))")
 
-
 function init!(obj::AdaDelta, weights)
     obj.initialized && error("already initialized")
     obj.sqgr = zeros(weights)
@@ -58,4 +57,32 @@ function update!{T<:FP}(obj::AdaDelta, weights::Vector{T}, gr::Vector{T})
     @devec obj.up[:] = - sqrt(obj.squp .+ obj.eps) ./ sqrt(obj.sqgr .+ obj.eps) .* gr #line 5
     @devec obj.squp[:] = obj.rho .* obj.squp + (1.0 - obj.rho) .* obj.up  .* obj.up #line 6
     add!(weights, obj.up) #line 7
+end
+
+type AdaGrad{T<:FP} <: AbstractSGD
+    eta::Float64
+    sqgr::Vector{T}
+    initialized::Bool
+    function AdaGrad(eta::Float64)
+        eta > 0.0 || error("eta should be positive")
+        obj = new()
+        obj.eta = eta
+        obj.initialized = false
+        obj
+    end
+end
+
+AdaGrad(eta::Float64) = AdaGrad{Float64}(eta)
+
+function init!(obj::AdaGrad, weights)
+    obj.initialized && error("already initialized")
+    obj.sqgr = zeros(weights)
+    obj.initialized = true
+end
+
+function update!{T<:FP}(obj::AdaGrad, weights::Vector{T}, gr::Vector{T})
+    obj.initialized || init!(obj, weights)
+    @devec obj.sqgr[:] += gr .* gr
+    @devec weights[:] -= obj.eta ./ sqrt(obj.sqgr) .* gr
+    weights
 end
