15819db5cf1ba4e46d6c242555dfaf0c7188dae6
Sam Lendle <sam.lendle@gmail.com>
2014-05-22 22:36:29 -0700

    Refactor glms

--- src/glm.jl
+++ src/glm.jl
@@ -1,119 +1,6 @@
-export GLMModel, LogisticModel, LinearModel, QuantileModel, GLMLearner, update!,
-        grad!, grad_scratch!, predict!, loss, predict, GLMNetLearner, linpred
+export GLMLearner, update!, grad!, predict!, loss, predict, GLMNetLearner, linpred, linpred!
 
-
-##############
-## GLMModel ##
-##############################
-## Used to specify GLM kind ##
-##############################
-abstract GLMModel
-#Subtypes should define the loss, predict!, and grad_scratch! functions
-# if the defaults are not appropriate.
-
-
-function linpred!(pr::DenseVector{Float64},
-                  coefs::Vector{Float64},
-                  x::Matrix{Float64};
-                  offset::Vector{Float64} = emptyvector(Float64))
-        A_mul_B!(pr, x, coefs)
-        if !isempty(offset)
-            add!(pr, offset)
-        end
-        pr
-end
-
-#
-# default predict! for GLMModel uses identity link
-#
-predict!(m::GLMModel,
-        pr::DenseVector{Float64},
-        coefs::Vector{Float64},
-        x::Matrix{Float64};
-        offset::Vector{Float64} = emptyvector(Float64)) = linpred!(pr, coefs, x; offset=offset)
-
-
-# grad_scratch! calculates the gradient without allocating new memory
-# for the residual vector.
-# resid is scratch space and should have dims equal to size(y)
-# Default is appropriate for GLMs with canonical link
-function grad_scratch!(m::GLMModel,
-                      gr::Vector{Float64},
-                      coefs::Vector{Float64},
-                      x::Matrix{Float64},
-                      y::Vector{Float64},
-                      resid::Vector{Float64};
-                      offset::Vector{Float64} = emptyvector(Float64))
-    predict!(m, resid, coefs, x; offset=offset)
-    subtract!(resid, y) # resid = prediction - y
-    alpha = 2.0/size(x, 1)
-    BLAS.gemv!('T', alpha, x, resid, 0.0, gr) #grad \propto x'resid = -x'(y - pred)
-    gr
-end
-
-grad!(m::GLMModel,
-    gr::Vector{Float64},
-    coefs::Vector{Float64},
-    x::Matrix{Float64},
-    y::Vector{Float64};
-    offset::Vector{Float64} = emptyvector(Float64)) = grad_scratch!(m, gr, coefs, x, y, similar(y), offset=offset)
-
-#########
-## OLS ##
-#########
-type LinearModel <: GLMModel end
-
-loss(m::LinearModel, pr::DenseVector{Float64}, y::Vector{Float64}) = meansqdiff(pr, y)
-
-
-#########################
-## Logistic regression ##
-#########################
-type LogisticModel <: GLMModel end
-
-function predict!(m::LogisticModel,
-                           pr::DenseVector{Float64},
-                           coefs::Vector{Float64},
-                           x::Matrix{Float64};
-                           offset::Vector{Float64} = emptyvector(Float64))
-    linpred!(pr, coefs, x; offset=offset)
-    map1!(LogisticFun(), pr)
-    pr
-end
-
-
-loss(m::LogisticModel, pr::DenseVector{Float64}, y::Vector{Float64}) = mean(NBLL(), pr, y)
-
-#########################
-## Quantile regression ##
-#########################
-type QuantileModel <: GLMModel
-    tau :: Float64
-end
-
-QuantileModel() = QuantileModel(0.5)
-
-function grad_scratch!(m::QuantileModel,
-                               gr::Vector{Float64},
-                               coefs::Vector{Float64},
-                               x::Matrix{Float64},
-                               y::Vector{Float64},
-                               scratch::Vector{Float64};
-                               offset::Vector{Float64} = emptyvector(Float64))
-    predict!(m, scratch, coefs, x; offset=offset)
-    subtract!(scratch, y)
-    map1!(Qgrad(), scratch, m.tau)
-    alpha = 2.0/size(x, 1)
-    BLAS.gemv!('T', alpha, x, scratch, 0.0, gr)
-    gr
-end
-
-loss(m::QuantileModel, pr::DenseVector{Float64}, y::Vector{Float64}) = meanabsdiff(pr, y)
-
-
-##################
-## End GLMModel ##
-##################
+include("glmmodel.jl")
 
 abstract AbstractGLMLearner <: Learner
 
@@ -130,7 +17,8 @@ linpred!(obj::AbstractGLMLearner, pr::DenseVector{Float64}, x::Matrix{Float64}; 
 linpred(obj::AbstractGLMLearner, x::Matrix{Float64}; offset=emptyvector(Float64)) =
     linpred!(obj, Array(Float64, size(x, 1)), x, offset=offset)
 
-
+loss(obj::AbstractGLMLearner, x::Matrix{Float64}, y::Vector{Float64}; offset=emptyvector(Float64)) =
+    loss(obj.m, predict(obj, x), y)
 
 ################################
 ## GLM without regularization ##
@@ -241,14 +129,11 @@ function update!(obj::GLMNetLearner, x::Matrix{Float64}, y::Vector{Float64})
         map!(Subtract(), obj.gr2, obj.lambda1, obj.gr)
         # then store obj.gr + lambda1 in obj.gr
         add!(obj.gr, obj.lambda1)
-        # update the weights for both gradients in mu and nu
-        update!(obj.optimizer, obj.mu, obj.gr)
+        update!(obj.optimizer, obj.mu, obj.gr) #update weights for both grads in mu, nu
         update!(obj.optimizer2, obj.nu, obj.gr2)
-        #truncate truncate temp weights mu and nu at 0.0
-        map1!(MaxFun(), obj.mu, 0.0)
+        map1!(MaxFun(), obj.mu, 0.0) #trunc temp weights at 0.0
         map1!(MaxFun(), obj.nu, 0.0)
-        #calculat the final weights as mu - nu
-        map!(Subtract(), obj.coefs, obj.mu, obj.nu)
+        map!(Subtract(), obj.coefs, obj.mu, obj.nu) #calc final weights as mu - nu
     else
         #if no l1 regularization (so either only l2, or no reg at all)
         #just do standard update
