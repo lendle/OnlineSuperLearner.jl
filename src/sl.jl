1cf621c3f6a17144037c4a17a4f54fe8bb379986
Sam Lendle <sam.lendle@gmail.com>
2014-06-04 23:07:25 -0700

    add calc_losses function

--- src/sl.jl
+++ src/sl.jl
@@ -1,10 +1,8 @@
-#jewel module OnlineSuperLearner
-
-export SuperLearner
+export SuperLearner, calc_losses
 
 type SuperLearner <: Learner
     candidates::Vector{Learner}
-    combiner::AbstractGLMLearner
+    combiner::GLMLearner
     Z::Matrix{Float64}
     t::Int
     initialized::Bool
@@ -16,6 +14,20 @@ type SuperLearner <: Learner
         obj.initialized = false
         obj
     end
+end
+
+function Base.show(io::IO, sl::SuperLearner)
+  Base.print(io, "Candidates: ")
+  for cand in sl.candidates
+      print(io, "\n\t")
+      show(io, cand)
+  end
+  Base.print(io, "\nCombiner:\n\t")
+  Base.show(io, sl.combiner)
+  if sl.combiner.initialized
+    Base.println(io, "\nWeights: ")
+    Base.println(io, sl.combiner.coefs)
+  end
 end
 
 function init!(obj::SuperLearner, n)
@@ -44,20 +56,30 @@ function update!(obj::SuperLearner, x::Matrix{Float64}, y::Vector{Float64})
     obj.initialized || init!(obj, size(x, 1))
     obj.t += 1
     if obj.t > 1
+        #candidates must be updated at least once before updating combination
         update_combination!(obj, x, y)
     end
     update_candidates!(obj, x, y)
 end
 
-function predict!(obj::SuperLearner, pr::DenseVector{Float64}, x::Matrix{Float64})
-    if size(x, 1) == size(obj.Z, 1)
-        Z = obj.Z
-    else
-        Z = Array(Float64, size(x, 1), length(obj.candidates))
-    end
+#if obj.Z is the right size, return it. Otherwise allocate a new matrix and return that.
+get_Z(obj::SuperLearner, x::Matrix{Float64}) = size(x, 1) == size(obj.Z, 1)? obj.Z: Array(Float64, size(x, 1), length(obj.candidates))
 
+function predict!(obj::SuperLearner, pr::DenseVector{Float64}, x::Matrix{Float64}, Z::Matrix{Float64}=get_Z(obj, x))
     for (j, cand) in enumerate(obj.candidates)
         predict!(cand, view(Z, :, j), x)
     end
     predict!(obj.combiner, pr, Z)
+end
+
+function calc_losses(obj::SuperLearner, x::Matrix{Float64}, y::Vector{Float64})
+    pr = Array(Float64, size(x, 1))
+    losses = Array(Float64, length(obj.candidates) + 1)
+    Z = get_Z(obj, x)
+    predict!(obj, pr, x, Z)
+    for (j, cand) in enumerate(obj.candidates)
+        losses[j] = loss(obj.combiner.m, view(Z, :, j), y)
+    end
+    losses[end] = loss(obj.combiner.m, pr, y)
+    losses
 end
