bb11e5bd1d0fed2a689f7d87337543279a989556
Sam Lendle <sam.lendle@gmail.com>
2014-06-10 14:16:42 -0700

    Try to add coverage to tests

--- test/test_sl.jl
+++ test/test_sl.jl
@@ -1,5 +1,6 @@
 cands  = [GLMLearner(LogisticModel(), SimpleSGD(1.0, 1.0)),
-          GLMLearner(LogisticModel(), SimpleSGD(1.0, 0.1))]
+          GLMLearner(LogisticModel(), SimpleSGD(1.0, 0.1)),
+          GLMNetLearner(LogisticModel(), SimpleSGD(1.0, 1.0), 1.0, 1.0)]
 
 combiner = GLMLearner(LogisticModel(), SimpleSGD(1.0, 1.0))
 
